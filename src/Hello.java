3eee3c277eeaefe19364bb9508dff981a9f35d42
wiliami <wiliaup97@gmail.com>
2022-02-22 22:59:42 -0400

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) throws Exception {
+        String name = "Dudu Oliveira";
+        int idade = 24;// idade
+        float altura = 1.45f;// altura
+        String sexo = "masculino";// sexo
+        String hobby = "tocar guitarra";// hobby
+        System.out.println(name + " tem " + idade + " anos e sua altura é " + altura + " e seu sexo é " + sexo + " e seu hobby é "+ hobby);
+        System.out.println("vou dormir já tá na hora!");
+    }
+}
+
+
